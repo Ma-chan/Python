ebd15c0db48c4c82f891c4b7c65a9533d1faefa3
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2018-12-09 20:11:57 +0900

    Update getNum.py

--- getNum.py
+++ getNum.py
@@ -1,13 +1,14 @@
 def getNum(a):
-   b_num = []
-   n=0
-   for i in range(len(a)):
-      if a[i] == n:
-       for n in range(0, 9):
-        print('True')
-        b_num += [int(a[i])]
+    b_num = []
+    n=0
+    b=0
+    for i in range(0, len(a)):
+        print(a[i], n)
+        if a[i] == n:
+            for b in range(0,9):
+                print('True')
+                b_num += [a[i]]
+        else:
+            print('False')
+            break
         return b_num
-      else:
-        print('False')
-        return b_num
-        break
