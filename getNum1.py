511847a133e0a3531730c80dc3f7eed5a4dc24a4
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2018-12-10 15:02:15 +0900

    Update getNum1.py

--- getNum1.py
+++ getNum1.py
@@ -2,17 +2,21 @@ def getNum(a):
     b_num = []
     n=0
     for i in range(0, len(a)):
-        print(a[i], n)
-        if a[i] == n:
-            while n<= 9:
-                print('True')
-                b_num += [a[i]]
-        else:
-            print('False')
-            break
-        return b_num
+        while n <= 9:
+            if a[i] == n:
+              print('True')
+              print(a[i],n)
+              b_num += a[i]
+            elif str(a[i]) == '+':
+                print('False')
+                break
+            elif str(a[i]) == '-':
+                print('False')
+                break
+    return b_num
 
     b_remain = []
     for b in range(k,len(a)):
         b_remain += [a[b]]
+        print(b_remain)
     return b_remain
