2c200601ebc110b35a75bec41acb14faef566e52
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2018-12-10 14:41:24 +0900

    Update getNum.py

--- getNum.py
+++ getNum.py
@@ -2,12 +2,12 @@ def getNum(a):
     b_num = []
     n=0
     for i in range(0, len(a)):
-        print(a[i], n)
-        if a[i] == n:
-            while n<= 9:
-                print('True')
-                b_num += [a[i]]
-        else:
-            print('False')
-            break
-        return b_num
+        for n in range(0,9):
+            if int(a[i]) == n:
+              print(a[i], n)
+              print('True')
+              b_num.extend(str(a[i]))
+            elif str(a[i]) == '+':
+                print('False')
+                break
+    return b_num
