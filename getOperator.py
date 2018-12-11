20de4eafc47fc5f44210848b455a337e6d0a952e
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2018-12-11 18:36:27 +0900

    Update getOperator.py

--- getOperator.py
+++ getOperator.py
@@ -1,16 +1,15 @@
 def getOperator(s):
     a_op=[]
     a_remain=[]
-    
+
     for i in range(0,len(s)):
-        if s[i] == "+":
-            a_op += s[i]
-        elif s[i] == "-":
-            a_op += s[i]
+        if s[i] == '+':
+          a_op = s[i]
+
+        elif s[i] == '-':
+          a_op = s[i]
         else:
-            i=
-            break
-    for a in range(k,len(s)):
-               
-    return a_op
-    return a_remain
+          a_remain = s[i:]
+          i=i+1
+          break
+    return a_op,a_remain
