f792ddf0ab163526f6aa1d5bd296bb31e65ab1d9
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2018-12-11 17:35:21 +0900

    Update getNum1.py

--- getNum1.py
+++ getNum1.py
@@ -1,22 +1,83 @@
-def getNum(a):
+def getNum(s):
     b_num = []
-    n=0
-    for i in range(0, len(a)):
-        while n <= 9:
-            if a[i] == n:
-              print('True')
-              print(a[i],n)
-              b_num += a[i]
-            elif str(a[i]) == '+':
-                print('False')
-                break
-            elif str(a[i]) == '-':
-                print('False')
-                break
-    return b_num
-
     b_remain = []
-    for b in range(k,len(a)):
-        b_remain += [a[b]]
-        print(b_remain)
-    return b_remain
+    for i in range(0,len(s)):
+        if s[i] == '0':
+            b_num += s[i]
+            i = i + 1
+        elif s[i] == '1':
+            b_num += s[i]
+            i = i + 1
+        elif s[i] == '2':
+            b_num += s[i]
+            i = i + 1
+        elif s[i] == '3':
+            b_num += s[i]
+            i = i + 1
+        elif s[i] == '4':
+            b_num += s[i]
+            i = i + 1
+        elif s[i] == '5':
+            b_num += s[i]
+            i = i + 1
+        elif s[i] == '6':
+            b_num += s[i]
+            i = i + 1
+        elif s[i] == '7':
+            b_num += s[i]
+            i = i + 1
+        elif s[i] == '8':
+            b_num += s[i]
+            i = i + 1
+        elif s[i] == '9':
+            b_num += s[i]
+            i = i + 1
+        elif s[i] == '+':
+            b_remain += s[i]
+            i = i + 1
+            break
+        elif s[i] == '-':
+            b_remain += s[i]
+            i = i + 1
+            break
+    for i in range(i,len(s)):
+        if s[i] == '0':
+            b_remain += s[i]
+            i = i + 1
+        elif s[i] == '1':
+            b_remain += s[i]
+            i = i + 1
+        elif s[i] == '2':
+            b_remain += s[i]
+            i = i + 1
+        elif s[i] == '3':
+            b_remain += s[i]
+            i = i + 1
+        elif s[i] == '4':
+            b_remain += s[i]
+            i = i + 1
+        elif s[i] == '5':
+            b_remain += s[i]
+            i = i + 1
+        elif s[i] == '6':
+            b_remain += s[i]
+            i = i + 1
+        elif s[i] == '7':
+            b_remain += s[i]
+            i = i + 1
+        elif s[i] == '8':
+            b_remain += s[i]
+            i = i + 1
+        elif s[i] == '9':
+            b_remain += s[i]
+            i = i + 1
+        elif s[i] == '+':
+            b_remain += s[i]
+            i = i + 1
+        elif s[i] == '-':
+            b_remain += s[i]
+            i = i + 1
+        else:
+            break
+    return b_num,b_remain
+
