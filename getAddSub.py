12fb772522cda164c41e227010045e2faf19fcbb
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2018-12-12 18:29:23 +0900

    Update and rename getNum2.py to getAddSub.py

--- getAddSub.py
+++ getAddSub.py
@@ -1,13 +1,11 @@
-def getNum(s):
-    b_num = []
-    b_remain=[]
+def getAddSub(s):
+    b_num = ''
+    b_remain= ''
     for i in range(0, len(s)):
         if s[i] == '+': break
         elif s[i] == '-': break
-        elif s[i] == '*': break
-        elif s[i] == '/': break
         else:
           b_num += s[i]
           i=i+1
-    return
-    b_num
+        b_remain = s[i:]
+    return b_num,b_remain
