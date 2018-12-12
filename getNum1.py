22663a0d1cc7ecae256230b10f57a1b76fac72ee
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2018-12-12 21:35:04 +0900

    Update getNum1.py

--- getNum1.py
+++ getNum1.py
@@ -1,41 +1,38 @@
 def getNum(s):
-    b_num = []
-    b_remain=[]
-    for i in range(0,len(s)):
-        if s[i] == [0-9]:
-            b_num += s[i]
-            i = i + 1
-        elif s[i] == '1':
-            b_num += s[i]
-            i = i + 1
-        elif s[i] == '2':
-            b_num += s[i]
-            i = i + 1
-        elif s[i] == '3':
-            b_num += s[i]
-            i = i + 1
-        elif s[i] == '4':
-            b_num += s[i]
-            i = i + 1
-        elif s[i] == '5':
-            b_num += s[i]
-            i = i + 1
-        elif s[i] == '6':
-            b_num += s[i]
-            i = i + 1
-        elif s[i] == '7':
-            b_num += s[i]
-            i = i + 1
-        elif s[i] == '8':
-            b_num += s[i]
-            i = i + 1
-        elif s[i] == '9':
-            b_num += s[i]
-            i = i + 1
-        else:
-            b_remain = s[i:]
-            b_num = "".join(b_num)
-            print(b_remain)
-            break
-    print(b_num)
-    return b_num,b_remain
+  b_num = ""
+  b_remain= ""
+  for i in range(0,len(s)):
+      if s[i] == "0":
+          b_num += s[i]
+          i = i + 1
+      elif s[i] == "1":
+          b_num += s[i]
+          i = i + 1
+      elif s[i] == "2":
+          b_num += s[i]
+          i = i + 1
+      elif s[i] == "3":
+          b_num += s[i]
+          i = i + 1
+      elif s[i] == "4":
+          b_num += s[i]
+          i = i + 1
+      elif s[i] == "5":
+          b_num += s[i]
+          i = i + 1
+      elif s[i] == "6":
+          b_num += s[i]
+          i = i + 1
+      elif s[i] == "7":
+          b_num += s[i]
+          i = i + 1
+      elif s[i] == "8":
+          b_num += s[i]
+          i = i + 1
+      elif s[i] == "9":
+          b_num += s[i]
+          i = i + 1
+      else:
+          b_remain = s[i:]
+          break
+  return b_num,b_remain
