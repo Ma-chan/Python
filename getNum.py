865baefcc713c2a40b1688fc23a4ba32099eff05
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2018-12-12 16:33:03 +0900

    Update getNum.py

--- getNum.py
+++ getNum.py
@@ -1,13 +1,36 @@
 def getNum(s):
-    b_num = []
-    n=0
-    for i in range(0, len(s)):
-          if s[i] == n:
-           while n <=9:
-              print('True')
-              print(s[i],n)
-              b_num.extend(n)
-          else:
-              print('False')
-              break
-    return b_num
+   b_num = ''
+   for i in range(0,len(s)):
+       if s[i] == '0':
+           b_num += s[i]
+           i=i+1
+       elif s[i] == '1':
+           b_num += s[i]
+           i=i+1
+       elif s[i] == '2':
+           b_num += s[i]
+           i=i+1
+       elif s[i] == '3':
+           b_num += s[i]
+           i = i + 1
+       elif s[i] == '4':
+           b_num += s[i]
+           i = i + 1
+       elif s[i] == '5':
+           b_num += s[i]
+           i = i + 1
+       elif s[i] == '6':
+           b_num += s[i]
+           i = i + 1
+       elif s[i] == '7':
+           b_num += s[i]
+           i = i + 1
+       elif s[i] == '8':
+           b_num += s[i]
+           i = i + 1
+       elif s[i] == '9':
+           b_num += s[i]
+           i = i + 1
+       else:
+           break
+   return b_num
